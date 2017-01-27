92b5d35cc4201eb815e9ddf622b5824a5955c32b
Tushar Gupta <tushar.gupta.cse@gmail.com>
2017-01-26 21:29:30 -0500

    Second started

--- test/app.js
+++ test/app.js
@@ -1,62 +1,67 @@
-(function() {
-    angular
-        .module("TestApp", [])
-        .controller("TestController", TestController)
-        .config(config)
-        .filter('reverse', function() {
-            return function(items) {
-                return items.slice().reverse();
-            };
-        });
+module.exports = function(app)
+{
+    app.get("/api/test", findAllMessages);
+    app.post("/api/test", createMessage);
+    app.delete("/api/test/:id", deleteMessage);
 
-    function config($httpProvider) {
-        console.log($httpProvider);
+    var connectionString = 'mongodb://127.0.0.1:27017/test';
+
+    if(process.env.MLAB_USERNAME_WEBDEV) {
+        var username = process.env.MLAB_USERNAME_WEBDEV;
+        var password = process.env.MLAB_PASSWORD_WEBDEV;
+
+        connectionString = 'mongodb://'+
+            username + ':' +
+            password +
+            '@ds157268.mlab.com:57268/heroku_nh37fqq4';
     }
-    
-    function TestController($http) {
-        var vm = this;
-        vm.createMessage = createMessage;
-        vm.deleteMessage = deleteMessage;
+    console.log(connectionString);
 
-        function init() {
-            findAllMessages();
-        }
-        init();
+    var mongoose = require("mongoose");  // npm install mongoose --save
+    mongoose.connect(connectionString);
 
-        function createMessage(message) {
-            vm.message = "";
-            var obj = {
-                message: message
-            };
-            $http.post("/api/test", obj)
-                .then(
-                    findAllMessages,
-                    function(err) {
-                        vm.error = err;
-                    }
-                );
-        }
+    var TestSchema = mongoose.Schema({
+        message: String
+    }, {collection: 'messages'});
 
-        function deleteMessage(message) {
-            $http.delete("/api/test/" + message._id)
-                .then(
-                    findAllMessages,
-                    function(err) {
-                        vm.error = err;
-                    }
-                );
-        }
+    var TestModel = mongoose.model("TestModel", TestSchema);
 
-        function findAllMessages() {
-            $http.get("/api/test")
-                .then(
-                    function(response) {
-                        vm.messages = response.data;
-                    },
-                    function(err) {
-                        vm.error = err;
-                    }
-                );
-        }
+    function findAllMessages(req, res) {
+        TestModel
+            .find() // select * from messages -- where 
+            .then(
+                function(tests) {
+                    res.json(tests);
+                },
+                function(err) {
+                    res.status(400).send(err);
+                }
+            );
     }
-})();
+
+    function createMessage(req, res) {
+        TestModel
+            .create(req.body)
+            .then(
+                function(test) {
+                    res.json(test);
+                },
+                function(err) {
+                    res.status(400).send(err);
+                }
+            );
+    }
+
+    function deleteMessage(req, res) {
+        TestModel
+            .remove({_id: req.params.id})
+            .then(
+                function(result) {
+                    res.json(result);
+                },
+                function(err) {
+                    res.status(400).send(err);
+                }
+            );
+    }
+};
