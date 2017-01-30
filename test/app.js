58ec9ab6191c94618505a682fb8ecf16ad327c4a
Tushar Gupta <tushar.gupta.cse@gmail.com>
2017-01-30 15:24:54 -0500

    Added pages for second assignment

--- test/app.js
+++ test/app.js
@@ -6,14 +6,12 @@ module.exports = function(app)
 
     var connectionString = 'mongodb://127.0.0.1:27017/test';
 
-    if(process.env.MLAB_USERNAME_WEBDEV) {
-        var username = process.env.MLAB_USERNAME_WEBDEV;
-        var password = process.env.MLAB_PASSWORD_WEBDEV;
-
-        connectionString = 'mongodb://'+
-            username + ':' +
-            password +
-            '@ds157268.mlab.com:57268/heroku_nh37fqq4';
+    if(process.env.MLAB_USERNAME) {
+        connectionString = process.env.MLAB_USERNAME + ":" +
+            process.env.MLAB_PASSWORD + "@" +
+            process.env.MLAB_HOST + ':' +
+            process.env.MLAB_PORT + '/' +
+            process.env.MLAB_APP_NAME;
     }
     console.log(connectionString);
 
