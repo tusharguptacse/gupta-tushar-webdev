8caadb74012e24b24c5532285a4e838f1e9b25ab
Tushar Gupta <tushar.gupta.cse@gmail.com>
2017-02-15 17:24:28 -0500

    Widget pages converted to Angular

--- public/assignment/config.js
+++ public/assignment/config.js
@@ -67,20 +67,20 @@
                 controllerAs: "model"
             })
             // Widget Routes
-            .when("/user/:uid/website/:wid/page/:pid/widget", {
+            .when("/user/:uid/website/:wid/page/:pid/widget",{
                 templateUrl: "views/widget/templates/widget-list.view.client.html",
                 controller: "WidgetListController",
                 controllerAs: "model"
             })
-            .when("/user/:uid/website/:wid/page/:pid/widget/:wgid", {
-                templateUrl: "views/widget/templates/widget-edit.view.client.html",
-                // controller: "WidgetListController",
-                // controllerAs: "model"
-            })
-            .when("/user/:uid/website/:wid/page/:pid/widget/new", {
+            .when("/user/:uid/website/:wid/page/:pid/widget/new",{
                 templateUrl: "views/widget/templates/widget-chooser.view.client.html",
-                // controller: "WidgetListController",
-                // controllerAs: "model"
+                controller: "NewWidgetController",
+                controllerAs: "model"
+            })
+            .when("/user/:uid/website/:wid/page/:pid/widget/:wgid",{
+                templateUrl: "views/widget/templates/widget-edit.view.client.html",
+                controller: "EditWidgetController",
+                controllerAs: "model"
             });
 
         // $locationProvider.html5Mode(true);
--- public/assignment/services/widget.service.client.js
+++ public/assignment/services/widget.service.client.js
@@ -1,27 +1,122 @@
 (function () {
     angular
         .module("WebAppMaker")
-        .factory("WidgetService", WidgetService);
-
+        .service("WidgetService", WidgetService);
     function WidgetService() {
-        var widgets = [
-            { "_id": "123", "widgetType": "HEADER", "pageId": "321", "size": 2, "text": "GIZMODO"},
-            { "_id": "234", "widgetType": "HEADER", "pageId": "321", "size": 4, "text": "Lorem ipsum"},
-            { "_id": "345", "widgetType": "IMAGE", "pageId": "321", "width": "100%",
-                "url": "http://lorempixel.com/400/200/"},
-            { "_id": "456", "widgetType": "HTML", "pageId": "321", "text": "<p>Lorem ipsum</p>"},
-            { "_id": "567", "widgetType": "HEADER", "pageId": "321", "size": 4, "text": "Lorem ipsum"},
-            { "_id": "678", "widgetType": "YOUTUBE", "pageId": "321", "width": "100%",
-                "url": "https://youtu.be/AM2Ivdi9c4E" },
-            { "_id": "789", "widgetType": "HTML", "pageId": "321", "text": "<p>Lorem ipsum</p>"}
-        ];
+        var widgets =
+            [
+                {"_id": "123", "widgetType": "HEADER", "pageId": "321", "size": 2, "text": "GIZMODO"},
+                {"_id": "234", "widgetType": "HEADER", "pageId": "321", "size": 4, "text": "Lorem ipsum"},
+                {
+                    "_id": "345", "widgetType": "IMAGE", "pageId": "321", "width": "100%",
+                    "url": "http://lorempixel.com/400/200/"
+                },
+                {"_id": "456", "widgetType": "HTML", "pageId": "321", "text": "<p>Lorem ipsum</p>"},
+                {"_id": "567", "widgetType": "HEADER", "pageId": "321", "size": 4, "text": "Lorem ipsum"},
+                {
+                    "_id": "678", "widgetType": "YOUTUBE", "pageId": "321", "width": "100%",
+                    "url": "https://youtu.be/AM2Ivdi9c4E"
+                },
+                {"_id": "789", "widgetType": "HTML", "pageId": "321", "text": "<p>Lorem ipsum</p>"}
+            ];
+
+        var options =
+            [1, 2, 3, 4, 5, 6];
+
         var api = {
-            "findAllWidgets": findAllWidgets
+            createWidget: createWidget,
+            findWidgetsByPageId: findWidgetsByPageId,
+            findWidgetById: findWidgetById,
+            updateWidget: updateWidget,
+            deleteWidget: deleteWidget,
+            getOptions: getOptions
         };
         return api;
 
-        function findAllWidgets(pageId) {
-            return widgets;
+        function getNewWidgetId() {
+            var date = new Date();
+
+            var components = [
+                date.getYear(),
+                date.getMonth(),
+                date.getDate(),
+                date.getHours(),
+                date.getMinutes(),
+                date.getSeconds(),
+                date.getMilliseconds()
+            ];
+
+            var id = components.join("");
+
+            return id;
+        }
+
+        function createWidget(pageId, widget) {
+
+            var newWidget = widget;
+            newWidget._id = getNewWidgetId();
+            newWidget.pageId = pageId;
+
+            widgets.push(newWidget);
+
+            return angular.copy(newWidget);
+        }
+
+        function findWidgetsByPageId(pageId) {
+            result = [];
+
+            for (var w in widgets) {
+
+                widget = widgets[w];
+
+                if (widget.pageId === pageId) {
+                    result.push(widget);
+                }
+            }
+            return result;
+        }
+
+        function findWidgetById(widgetId) {
+            widgetFound = null;
+
+            for (var w in widgets) {
+                widget = widgets[w];
+                if (widget._id === widgetId) {
+                    widgetFound = widget;
+                    break;
+                }
+            }
+            return angular.copy(widgetFound);
+        }
+
+        function updateWidget(widgetId, widget) {
+            var updateSuccessful = false;
+            for (var w in widgets) {
+                curwidget = widgets[w];
+                if (curwidget._id === widgetId) {
+                    widgets[w] = widget;
+                    updateSuccessful = true;
+                    break;
+                }
+            }
+            return updateSuccessful;
+        }
+
+        function deleteWidget(widgetId) {
+            var deleteSuccessful = false;
+
+            for (var index = 0; index < widgets.length; index++) {
+                if (widgets[index]._id === widgetId) {
+                    widgets.splice(index, 1);
+                    deleteSuccessful = true;
+                    break;
+                }
+            }
+            return deleteSuccessful;
+        }
+
+        function getOptions() {
+            return options;
         }
     }
 })();
